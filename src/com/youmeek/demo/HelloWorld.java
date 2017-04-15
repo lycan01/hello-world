36ade49e79b55778e4e34ccc13f6191d7f80ca44
unknown <>
2017-04-15 19:20:45 +0800

    Initial commit

---
+++ src/com/youmeek/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.youmeek.demo;
+
+/**
+ * Created by 18210 on 2017/4/15.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.print("Hello，World");
+    }
+}
